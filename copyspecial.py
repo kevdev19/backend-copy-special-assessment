882d81d895ae1f8a11d5242a37a644acb732bb0b
Kevin Blount <kvbchamp@gmail.com>
2020-06-18 13:28:38 -0400

    complete get_special_paths function

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Kevin Blount"
 
 import re
 import os
@@ -19,8 +19,14 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    dir_list = os.listdir(dirname)
+
+    special_paths = []
+    for file in dir_list:
+        match = re.search(r"__\w+__", file)
+        if match:
+            special_paths.append(os.path.abspath(os.path.join(dirname, file)))
+    return special_paths
 
 
 def copy_to(path_list, dest_dir):
@@ -39,9 +45,8 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='dir to look in for special files')
     ns = parser.parse_args(args)
-
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
 
@@ -49,8 +54,11 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
+    if not ns.from_dir:
+        parser.print_usage()
+        sys.exit(1)
 
-    # Your code here: Invoke (call) your functions
+    get_special_paths(ns.from_dir)
 
 
 if __name__ == "__main__":
